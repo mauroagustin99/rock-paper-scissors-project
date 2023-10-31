6a77d8fd6880fc7fbd7b0b22b0302afe17cfb663
mauroagustin99 <mauromarcosano@gmail.com>
2023-10-30 21:32:48 -0300

    Added "round" function and "game" function to make the game functional

--- script.js
+++ script.js
@@ -1,9 +1,91 @@
-console.log("hello world");
+const options = ["rock", "paper", "scissors"];
+let countUser = 0; let countComputer = 0;
 
-const options =["rock","paper","scissors"];
 
-function getComputerChoice(){
-    return console.log(options[(Math.floor(Math.random()*options.length))]);
-};
+function round(playerSelection, computerSelection) {
+    switch (playerSelection) {
+        case "rock":
+            if (computerSelection == "paper") {
+                countComputer += 1;
+                return ("You Lose! Paper beats Rock");
+            }
+            else if (computerSelection == "scissors") {
+                countUser += 1;
+                return ("You Win! Rock beats Scissors");
+            }
+            else {
+                return ("Tie");
+            }
+        case "paper":
+            if (computerSelection == "scissors") {
+                countComputer += 1;
+                return ("You Lose! Scissors beats Paper");
+            }
+            else if (computerSelection == "rock") {
+                countUser += 1;
+                return ("You Win! Paper beats Rock");
+            }
+            else {
+                return ("Tie");
+            }
+        case "scissors":
+            if (computerSelection == "rock") {
+                countComputer += 1;
+                return ("You Lose! Rock beats Scissors");
+            }
+            else if (computerSelection == "paper") {
+                countUser += 1;
+                return ("You Win! Scissors beats Paper");
+            }
+            else {
+                return ("Tie");
+            }
+    }
+}
 
-getComputerChoice();
+function game() {
+
+    //Computer's random choosing
+    function getComputerChoice() {
+        return (options[(Math.floor(Math.random() * options.length))]);
+    };
+
+    //The player chooses here without the possibility of making a mistake
+    let playerSelection;
+    let finalSelection;
+    do {
+        playerSelection = prompt("Choose an option: rock , paper or scissors.").toLowerCase();
+        if (options.includes(playerSelection)) {
+            finalSelection = playerSelection;
+        }
+        else {
+            alert("Invalid option, try again.");
+        }
+    }
+    while (finalSelection !== playerSelection);
+    console.log(finalSelection);
+    console.log(playerSelection);
+
+
+    let computerSelection = getComputerChoice();
+
+    console.log(round(finalSelection, computerSelection));
+    console.log("Score User: " + countUser + " || Score Computer: " + countComputer);
+
+
+}
+
+for (var i = 0; i <= 5; i++) {
+    game();
+}
+if (countUser>countComputer){
+    console.log("You are the winner!");
+}
+else if(countUser<countComputer){
+    console.log("The computer won!");
+}
+else{
+    console.log("It's a draw!");
+}
+
+
